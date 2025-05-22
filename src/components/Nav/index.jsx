da770419910dd8464111b42e17e8e0814226a20a
Anibal Sanchez <anibalnuma@gmail.com>
2025-05-22 10:10:02 -0400

    Update component Nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="flex justify-center items-center gap-6 bg-blue-400 w-full px-6 py-1 rounded-2xl text-white font-bold">
-      <Link to="/users">Users</Link>
+      <Link to="/users">Users List</Link>
       <Link to="/user-form">User Form</Link>
     </div>
   );
